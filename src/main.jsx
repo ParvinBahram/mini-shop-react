aa704bfd6c032cb5ee04be6d77dab72ee3dd1069
ParvinBahram <parvin.bahram91@gmail.com>
2026-09-07 22:51:28 +0330

    fixed cartContext import name

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
-import './index.css'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import CartProvider from './components/cartContext.jsx'
+import "./index.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import CartProvider from "./components/CartContext.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <CartProvider>
-  <BrowserRouter>
-  <StrictMode>
-    <App />
-  </StrictMode>
-  </BrowserRouter>
-  </CartProvider>
-)
+    <BrowserRouter>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </BrowserRouter>
+  </CartProvider>,
+);
